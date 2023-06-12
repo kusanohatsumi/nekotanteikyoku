5f6c17713b42b825eb8946c02ca696c7042428bf
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-06-12 15:51:12 +0900

    add:トップページを追加
    各項目の見出しやsectionで囲んだ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main>
-      <h1>猫探偵局</h1>
+    <main className="h-full  my-0 mx-auto">
+      <h1>ねこ探偵局</h1>
+      <section>
+        <h2>
+          ようこそ
+          <br />
+          ねこ探偵局へ
+        </h2>
+      </section>
+      <section>
+        <h2>ぶろぐ</h2>
+      </section>
     </main>
   );
 }
