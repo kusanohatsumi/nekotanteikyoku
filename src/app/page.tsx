d685841feab9348875be40e6f8f42041b7d8d666
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-06-12 16:26:54 +0900

    update:text-center
    テキストを中央揃えにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 export default function Home() {
   return (
     <main className="h-full  my-0 mx-auto">
-      <h1>ねこ探偵局</h1>
+      <h1 className="text-center">ねこ探偵局</h1>
       <section>
-        <h2>
+        <h2 className="text-center">
           ようこそ
           <br />
           ねこ探偵局へ
         </h2>
       </section>
       <section>
-        <h2>ぶろぐ</h2>
+        <h2 className="text-center">ぶろぐ</h2>
       </section>
     </main>
   );
