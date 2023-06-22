c0f3a23a1fe119384734b905fe05e090bc753287
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-06-22 14:14:07 +0900

    update:headerにロゴとメニュー追加
    headerにロゴ画像とハンバーガーメニューを追加した

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/ui/header";
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
