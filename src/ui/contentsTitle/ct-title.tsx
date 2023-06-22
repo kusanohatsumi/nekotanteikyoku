1b08087103e7a71992ac5a853efec9e2cf9d2f48
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-06-22 16:44:11 +0900

    add:コンテンツタイトルの画像コンポーネント作成
    コンテンツの見出し部分の背景画像を使いまわせるようにコンポーネントにして、コンテンツ名の画像を入れるだけで見出しが完成するようにした

---
+++ src/ui/contentsTitle/ct-title.tsx
@@ -0,0 +1,37 @@
+"use client";
+import styled from "styled-components";
+
+const Ct_title = styled.div`
+  width: 95%;
+  height: 100px;
+  border-bottom: solid 1px #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+  &::before {
+    content: "";
+    display: block;
+    width: 5px;
+    height: 5px;
+    background-color: #fff;
+    border-radius: 50%;
+    position: absolute;
+    bottom: -2.5px;
+    left: -2px;
+  }
+  &::after {
+    content: "";
+    display: block;
+    width: 5px;
+    height: 5px;
+    background-color: #fff;
+    border-radius: 50%;
+    position: absolute;
+    bottom: -2.5px;
+    right: -2px;
+  }
+`;
+
+export default Ct_title;
