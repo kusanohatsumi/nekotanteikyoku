87ad664694d0a805e4813ae877e664dac9404c30
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-06-22 16:47:21 +0900

    update:コンテンツタイトル部分の変更
    コンポーネントの参照を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import BlogItem from "@/ui/blogItem";
+import Ct_BgImg from "@/ui/contentsTitle/ct-bgImg";
+import Ct_title from "@/ui/contentsTitle/ct-title";
 
 export default function Home() {
   return (
-    <main className="h-full  my-0 mx-auto ">
+    <main className="h-full  my-0 mx-auto">
       <h1 className="text-center mb-3">ねこ探偵局</h1>
       <section>
         <h2 className="text-center mb-3">
@@ -12,7 +14,10 @@ export default function Home() {
         </h2>
       </section>
       <section>
-        <h2 className="text-center border-b-2 mb-3">ぶろぐ</h2>
+        <Ct_title className="m-auto pb-4">
+          <h2>{/* ここにコンテンツ名の画像を入れる */}</h2>
+          <Ct_BgImg />
+        </Ct_title>
         <a href="/">
           <BlogItem />
         </a>
