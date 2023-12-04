a0a2e4447209ac39359cf1cc13c6137c15bdcec6
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-12-04 15:05:48 +0900

    fix:エラーの修正

--- src/feat/microCMS/index.tsx
+++
@@ -1,8 +0,0 @@
-import { getList } from "@/lib/client";
-import { Goods } from "@/types/blog";
-
-export default async function MicroCMS() {
-	const { goods } = await getList();
-	const GoodsItem: Goods[] = goods.contents;
-	return GoodsItem;
-}
