4b1cf77c51e5e8784a418ef204fc2e9dca07c799
HatsumiKusano <kusano.hatsumi@gmail.com>
2024-01-15 16:52:15 +0900

    feat:top/goodsList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 import { client } from "@/lib/microcms";
 
-async function getContents() {
-	const response = await client.getList({
-		customRequestInit: {
-			cache: "no-store", // キャッシュを利用せずに常に新しいデータを取得する
-		},
-		endpoint: "goods",
-	});
-
-	return response.contents;
-}
-
 export default async function Home() {
 	const contents = await getContents();
 	return (
 		<>
-			<div className="firstview relative  flex flex-col items-center">
+			{/* ファーストビュー */}
+			<div className="firstview w-full relative  flex flex-col items-center">
 				<figure>
 					<Image
 						src="/images/title.png"
@@ -42,141 +32,52 @@ export default async function Home() {
 					/>
 				</figure>
 			</div>
-			<Link href="/static">Static</Link>
-			<ul>
-				{contents.map((item) => {
-					return (
-						<div key={item.id}>
-							<li>{item.name}</li>
+			{/* グッズ一覧 */}
+			<div className="container">
+				<div className="goods">
+					<section>
+						<h2 className="title">
+							<Image src="/title_goods.png" alt="sns" width={83} height={35} />
 							<Image
-								src={item.eyecatch.url}
-								alt={`${item.name}`}
-								width={100}
-								height={100}
+								src="/section_title.png"
+								alt="見出し装飾"
+								width={250}
+								height={60}
+								className="m-auto absolute"
 							/>
-						</div>
-					);
-				})}
-			</ul>
+						</h2>
+						<ul className="goodsList">
+							{contents.map((item) => {
+								return (
+									<li key={item.id} className="m-2">
+										<Image
+											src={item.eyecatch.url}
+											alt={item.title}
+											width="0"
+											height="0"
+											sizes="90vw"
+											style={{ width: "70%", height: "auto" }}
+										/>
+									</li>
+								);
+							})}
+						</ul>
+					</section>
+					<div className="more">
+						<Link href="/goods">もっとみる </Link>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
 
-// const Home = async () => {
-// 	const contents = await getContents();
-// 	return (
-// 		<ul>
-// 			{contents.map((item, i) => {
-// 				<div key={i}></div>;
-// 				return <div key={i}>{item.name}</div>;
-// 			})}
-// 		</ul>
-// 	);
-
-// 	return (
-// 		<>
-// 			<Link href="/static">Static</Link>
-// 			<div>{}</div>
-
-// 			{/* <FirstView />
-// 				<Contents className="m-auto py-44 ">
-// 					<About />
-// 					<section className="my-8">
-// 						<Ct_title className="m-auto">
-// 							<h2>
-// 								<Image
-// 									src="/title_sns.png"
-// 									alt="sns"
-// 									width={83}
-// 									height={35}
-// 									style={{ width: "auto" }}
-// 								/>
-// 							</h2>
-// 							<Ct_BgImg />
-// 						</Ct_title>
-// 						<List>
-// 							<SNS className="sns note">
-// 								<Link href="https://note.com/nice_daisy154" target="_blank">
-// 									<h3>note</h3>
-// 									<p>
-// 										ブログを投稿
-// 										<br />
-// 										しています
-// 									</p>
-// 								</Link>
-// 							</SNS>
-// 							<SNS className="sns instagram">
-// 								<Link
-// 									href="https://www.instagram.com/nekotanteikyoku/"
-// 									target="_blank"
-// 								>
-// 									<h3>instagram</h3>
-// 									<p>ここに各SNSの説明を表示</p>
-// 								</Link>
-// 							</SNS>
-// 							<SNS className="sns twitter">
-// 								<Link href="https://twitter.com/home?lang=ja" target="_blank">
-// 									<h3>twitter</h3>
-// 									<p>ここに各SNSの説明を表示</p>
-// 								</Link>
-// 							</SNS>
-// 							<SNS className="sns tiktok">
-// 								<Link href="#">
-// 									<h3>tiktok</h3>
-// 									<p>ここに各SNSの説明を表示</p>
-// 								</Link>
-// 							</SNS>
-// 						</List>
-// 					</section>
-// 					<section className="my-8">
-// 						<Ct_title className="m-auto">
-// 							<h2>
-// 								<Image
-// 									src="/title_goods.png"
-// 									alt="sns"
-// 									width={83}
-// 									height={35}
-// 								/>
-// 							</h2>
-// 							<Ct_BgImg />
-// 						</Ct_title>
-// 						<List><GoodsList data={data} /></List>
-// 						<More className="m-auto">
-// 							<Link href="/goods">もっとみる </Link>
-// 						</More>
-// 					</section>
-// 					<Contact>
-// 						<figure className="px-6 my-6 flex justify-between">
-// 							<Image
-// 								src="/title_contact.png"
-// 								alt="お問合せ"
-// 								width={200}
-// 								height={40}
-// 							/>
-// 							<Image src="/stamp.svg" alt="スタンプ" width={115} height={35} />
-// 						</figure>
-// 						<p className="px-6 leading-9">
-// 							ご質問やおすすめの猫スポットを募集しています。また、グッズやチラシ等のご依頼もお待ちしております！
-// 						</p>
-// 						<ContactBtn>
-// 							<Link
-// 								href="mailto:nekotanteikyoku@gmail.com"
-// 								className="w-full h-full flex items-center justify-center"
-// 								style={{ color: "#6B8970" }}
-// 							>
-// 								<Image
-// 									src="/icon_mail.svg"
-// 									alt="mailIcon"
-// 									width={20}
-// 									height={20}
-// 								/>
-// 								nekotanteikyoku@gmail.com
-// 							</Link>
-// 						</ContactBtn>
-// 					</Contact>
-// 					<footer className="text-center">©nekotanteikyoku</footer>
-// 				</Contents> */}
-// 		</>
-// 	);
-// };
-// export default Home;
+async function getContents() {
+	const response = await client.getList({
+		customRequestInit: {
+			cache: "no-store", // キャッシュを利用せずに常に新しいデータを取得する
+		},
+		endpoint: "goods",
+	});
+	return response.contents;
+}
