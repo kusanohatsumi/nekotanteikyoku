0bc61c0c31f046b3d7cce083439e573239295e8c
HatsumiKusano <kusano.hatsumi@gmail.com>
2024-01-18 11:49:59 +0900

    fix:use clientを削除

--- src/app/goods/[id]/page.tsx
+++ src/app/goods/[id]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import "../../../styles/goodsdetail.css";
 import { notFound } from "next/navigation";
 import { getGoodsDetail } from "@/lib/microcms";
@@ -7,7 +6,6 @@ import Link from "next/link";
 
 export default async function DetailPage({ params }: { params: any }) {
 	const post = await getGoodsDetail(params.id);
-	console.log(post);
 	console.log(post.url);
 
 	if (!post) {
